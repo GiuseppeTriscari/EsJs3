9bcaa1c7eba348b25467c81693e5a14283c76352
Triscari Giuseppe <triscari.giuseppe@osdbsesto>
2020-12-03 13:38:05 +0100

    ok

--- Js.js
+++ Js.js
@@ -1,22 +1,30 @@
 document.addEventListener("DOMContentLoaded", addButton);
 
-var add1;
-var add2;
-var sott1;
-var sott2;
-var molt1;
-var molt2;
-var div1;
-var div2;
+var num1;
+var num2;
+var ris;
 
-var addButton = function(){
-    var btn = document.createElement("button");
-    btn.innerHTML = "GARPEZ";
-    document.getElementById("body1").append(btn);
-    btn.addEventListener("click", change)
+
+var add = function(){
+    ris = parseInt(num1) + parseInt(num2);
+    console.log(ris);
 }
 
-var change=function (){
+var sott = function (){
+    console.log("SCISCO");
+    document.getElementById('div1').setAttribute("style", "background-color: cyan");
+    document.getElementById("span1").innerHTML = "<h1>CYAN SUS</h1>";
+
+}
+
+var div = function (){
+    console.log("SCISCO");
+    document.getElementById('div1').setAttribute("style", "background-color: cyan");
+    document.getElementById("span1").innerHTML = "<h1>CYAN SUS</h1>";
+
+}
+
+var molt = function (){
     console.log("SCISCO");
     document.getElementById('div1').setAttribute("style", "background-color: cyan");
     document.getElementById("span1").innerHTML = "<h1>CYAN SUS</h1>";
